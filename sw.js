4655e17b4cd21575555a4d9a453d81e8eec9e8a8
hns140412-glitch <hns140412@gmail.com>
2026-09-07 18:51:13 +0900

    chore: cache Hide bridge and preserve safe update activation

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE="hide-seek-runtime-20260907-v2";
+const CACHE="hide-seek-runtime-20260907-v3";
 const CORE=[
-  "./","./index.html","./styles.css","./hide-runtime.css","./app.js","./hide-runtime.js","./manifest.json","./assets/asset-map.json",
+  "./","./index.html","./styles.css","./hide-runtime.css","./hide-bridge.css","./app.js","./hide-runtime.js","./hide-bridge.js","./manifest.json","./assets/asset-map.json",
   "./assets/icons/icon-180x180.png","./assets/icons/icon-192x192.png","./assets/icons/icon-512x512.png",
   "./assets/backgrounds/academy.png","./assets/backgrounds/park.png","./assets/backgrounds/bookstore.png","./assets/backgrounds/cafe.png","./assets/backgrounds/classroom.png","./assets/backgrounds/station.png",
   "./assets/characters/guide_default.png","./assets/characters/guide_smile.png","./assets/characters/guide_hint.png","./assets/characters/guide_note.png","./assets/characters/guide_radio.png","./assets/characters/guide_fever.png","./assets/characters/guide_focus.png","./assets/characters/guide_cheer.png","./assets/characters/rabbit.png","./assets/characters/fennec.png","./assets/characters/sloth.png",
